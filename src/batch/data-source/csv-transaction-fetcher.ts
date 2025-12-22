55de1a6848275fc992530a50d6189c18b9f08890
adalyn.kim <adalyn.kim@goorm.io>
2025-12-22 12:44:33 +0900

    fix: fs/promise import 함수 변경

--- src/batch/data-source/csv-transaction-fetcher.ts
+++ src/batch/data-source/csv-transaction-fetcher.ts
@@ -1,5 +1,5 @@
 import { Transaction, TransactionFetcher } from '../type/transaction';
-import fs from 'fs/promises';
+import { readFile } from 'fs/promises';
 import { parse } from 'csv-parse/sync';
 import { BatchLogger } from '../../log/type/batch-logger';
 
@@ -8,7 +8,7 @@ export class CsvTransactionFetcher implements TransactionFetcher {
 
   async fetch(page: number): Promise<Transaction[]> {
     try {
-      const raw = await fs.readFile(this.filePath, 'utf-8');
+      const raw = await readFile(this.filePath, 'utf-8');
       const records = parse(raw, { columns: true, skip_empty_lines: true });
       return records as Transaction[];
     } catch (error) {
